a6dc0e119250977b8544b64222de3229ea3ef29c
melissamcl <mcl.melissa.m@gmail.com>
2023-04-18 15:57:21 -0400

    set up chat page, no functionality

--- src/client/chatPage.tsx
+++ src/client/chatPage.tsx
@@ -6,19 +6,45 @@ const Conversation: React.FC = () => {
   const sampleData = [
     {
       sender: 'gpt',
-      message: 'chat-gpt-message',
+      message: `(chat-gpt-message) It is a long established fact that a reader will be distracted 
+      by the readable content of a page when looking at its layout. The point of using Lorem Ipsum 
+      is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here
+      , content here', making it look like readable English. Many desktop publishing packages and web 
+      page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' 
+      will uncover many web sites still in their infancy. Various versions have evolved over the years, 
+      sometimes by accident, sometimes on purpose (injected humour and the like).`,
     },
     {
       sender: 'user',
-      message: 'user-message',
+      message: `(user-message) It is a long established fact that a reader will be distracted 
+      by the readable content of a page when looking at its layout. The point of using Lorem Ipsum 
+      is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here'`,
     },
     {
       sender: 'gpt',
-      message: 'chat-gpt-message',
+      message: `(chat-gpt-message) It is a long established fact that a reader will be distracted 
+      by the readable content of a page when looking at its layout. The point of using Lorem Ipsum 
+      is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here
+      , content here', making it look like readable English. Many desktop publishing packages and web 
+      page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum'`,
     },
     {
       sender: 'user',
-      message: 'user-message',
+      message: `(user-message) It is a long established fact that a reader will be distracted`,
+    },
+    {
+      sender: 'gpt',
+      message: `(chat-gpt-message) It is a long established fact that a reader will be distracted 
+      by the readable content of a page when looking at its layout. The point of using Lorem Ipsum 
+      is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here
+      , content here', making it look like readable English. Many desktop publishing packages and web 
+      page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum'`,
+    },
+    {
+      sender: 'user',
+      message: `(user-message) It is a long established fact that a reader will be distracted 
+      by the readable content of a page when looking at its layout. The point of using Lorem Ipsum 
+      is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here'`,
     },
   ];
 
@@ -33,7 +59,7 @@ const NewChat: React.FC = () => {
   return (
     <div className="new-chat">
       <textarea />
-      <button>Send</button>
+      <button id="send-msg-button">→</button>
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -2,32 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './client/mainPage';
-<<<<<<< HEAD
 import ChatPage from './client/chatPage';
-import './index.scss';
-=======
-// import ChatPage from './client/chatPage';
 import Login from './client/login';
 import Signup from './client/signup';
-import App from './App'
-import './index.css'
-
->>>>>>> 43eb0d8586c56079719b73ee09eb6dfb779201ed
+import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-<<<<<<< HEAD
         <Route path="/chat" element={<ChatPage />} />
-=======
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/app" element={<App />} />
-        {/* <Route path="/chat" element={<ChatPage />} /> */}
-
->>>>>>> 43eb0d8586c56079719b73ee09eb6dfb779201ed
       </Routes>
     </Router>
   </React.StrictMode>
