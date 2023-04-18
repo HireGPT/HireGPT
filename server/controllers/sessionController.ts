80f7ed259498c88f4552d55cde96a35257e01fe7
Jason <jjhuang3@hotmail.com>
2023-04-18 11:38:31 -0700

    commit to code review my query

---
+++ server/controllers/sessionController.ts
@@ -0,0 +1,32 @@
+const query = require('../models/dbModel')
+
+
+const sessionController = {};
+
+sessionController.signUp = async (req, res, next) => {
+    const {email, pw} = req.body;
+
+    const find = `SELECT email FROM users
+                    WHERE email = ${email}`
+
+    try {
+        await query(find, (err, result) => {})
+    }
+
+    const insert = `INSERT INTO users (${email}, ${pw})
+    VALUES (email, pw)`
+
+    try {
+        await query(insert, (err, result) => {
+            if (err) {
+                return next('Error signing up', err);
+            } else {
+                res.locals.userData = result
+                return next();
+            }
+        });
+    } catch (error) {
+        return next('Error getting signing up', error)
+    }
+};
+
