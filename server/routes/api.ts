18ce65c07666215564dd453b32b6580484000d10
Nancy Yu <nancyyu09@gmail.com>
2023-04-19 11:31:31 -0700

    Completed getPersonalities endpoint
    Co-authored-by: Jamie Lee <jamieslee97@users.noreply.github.com>
    Co-authored-by: Jason Huang <jjhuang3@users.noreply.github.com>

--- server/routes/api.ts
+++ server/routes/api.ts
@@ -3,18 +3,18 @@ import { Request, Response } from 'express';
 import sessionController from '../controllers/sessionController';
 import userController from '../controllers/userController';
 import chatController from '../controllers/openAIController'
-
+import personalityController from '../controllers/personalityController'
 
 require('dotenv').config();
 
 const router = express.Router();
 
 router.post('/login',
-    userController.verifyUser, 
-    sessionController.startSession,
-    (req: Request, res: Response): Response => {
+  userController.verifyUser,
+  sessionController.startSession,
+  (req: Request, res: Response): Response => {
     return res.status(200).json(res.locals.users);
-    });
+  });
 
 router.post(
   '/signup',
@@ -33,7 +33,13 @@ router.get('/loggedIn', sessionController.isLoggedIn, (req, res) => {
   res.json({ message: 'you are logged in' })
 })
 
-router.post('/chat', chatController)
+router.post('/chat', chatController);
+
+router.get('/personality', personalityController.getPersonalities, (req, res) => {
+  res.status(200).json(res.locals.personalities);
+});
+
+
 
 
 export default router;
