bfad1985a36cec50cbced7f60370577ce6197314
Fred Kim <104876440+Fredkim21@users.noreply.github.com>
2023-04-18 18:56:20 -0700

    fixed the value prop

--- src/PersonProvider.tsx
+++ src/PersonProvider.tsx
@@ -7,9 +7,10 @@ interface PersonProviderProps {
 
 export const PersonProvider: React.FC<PersonProviderProps> = ({ children }) => {
   const [person, setPerson] = useState<PersonProps | null>(null);
+  const [persons, setPersons] = useState<Array<PersonProps>>([]); // Add this line
 
   return (
-    <PersonContext.Provider value={{ person, setPerson }}>
+    <PersonContext.Provider value={{ person, setPerson, persons, setPersons }}> // Update the value prop
       {children}
     </PersonContext.Provider>
   );
